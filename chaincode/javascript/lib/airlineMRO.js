375de0d4c1c5ef61a08f1430aa777e7e538c2e13
aalu1418 <aalu1418@gmail.com>
2020-04-07 19:27:26 -0400

    fixed bug

--- chaincode/javascript/lib/airlineMRO.js
+++ chaincode/javascript/lib/airlineMRO.js
@@ -2,15 +2,14 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-'use strict';
+"use strict";
 
-const { Contract } = require('fabric-contract-api');
+const { Contract } = require("fabric-contract-api");
 
 class airlineMRO extends Contract {
-
-  // Adding entires for verifying chaincode works
+    // Adding entires for verifying chaincode works
     async initLedger(ctx) {
-        console.info('============= START : Initialize Ledger ===========');
+        console.info("============= START : Initialize Ledger ===========");
         const test = [
             {
                 test: "check",
@@ -18,19 +17,22 @@ class airlineMRO extends Contract {
                 error: "none"
             },
             {
-                test: "all systems go"
-                status:"engine ignition",
+                test: "all systems go",
+                status: "engine ignition",
                 error: "nominal"
-            },
+            }
         ];
 
         for (let i = 0; i < test.length; i++) {
-            test[i].docType = 'user';
+            test[i].docType = "user";
             // await ctx.stub.putState('USER' + i, Buffer.from(JSON.stringify(test[i])));
-            await ctx.stub.putState((i+1).toString(), Buffer.from(JSON.stringify(test[i])));
-            console.info('Added <--> ', test[i]);
+            await ctx.stub.putState(
+                (i + 1).toString(),
+                Buffer.from(JSON.stringify(test[i]))
+            );
+            console.info("Added <--> ", test[i]);
         }
-        console.info('============= END : Initialize Ledger ===========');
+        console.info("============= END : Initialize Ledger ===========");
     }
 
     // //  Fetches individual's data for KYC information
@@ -58,23 +60,33 @@ class airlineMRO extends Contract {
 
     //  users approves companies to access their information
     async registerUser(ctx, user) {
-        console.log('======== START : Approve company for user data access ==========');
+        console.log(
+            "======== START : Approve company for user data access =========="
+        );
 
         //  `relations` created for 2-way access like if company id is given then list of users will be fetched  and vice versa too.
-        const relations = 'user';  //  relation type to be stored on blockchain (company~user or user~company)
-        const compositeKey = await ctx.stub.createCompositeKey(relations, [user.type.toString(), user.company.toString(), user.username.toString()]);  //  create company~user relation unique key
+        const relations = "user"; //  relation type to be stored on blockchain (company~user or user~company)
+        const compositeKey = await ctx.stub.createCompositeKey(relations, [
+            user.type.toString(),
+            user.company.toString(),
+            user.username.toString()
+        ]); //  create company~user relation unique key
 
         // Validations for composite keys created
         if (!compositeKey) {
-            throw new Error('compositeKey is null');
+            throw new Error("compositeKey is null");
         }
 
         user.aircraft = []; //initialize aircraft array for user
 
-        await ctx.stub.putState(compositeKey, Buffer.from(JSON.stringify(user)));    //  Store composite key relation for user
-        console.log('======== END : Relation of approved companies for users stored =========');
+        await ctx.stub.putState(
+            compositeKey,
+            Buffer.from(JSON.stringify(user))
+        ); //  Store composite key relation for user
+        console.log(
+            "======== END : Relation of approved companies for users stored ========="
+        );
     }
-
 }
 
 module.exports = airlineMRO;
