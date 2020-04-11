b19da3c6fe7645b090104e450244cd3e95afaa7f
aalu1418 <aalu1418@gmail.com>
2020-04-11 14:59:41 -0400

    update hours api endpoint

--- api/routes/flight.js
+++ api/routes/flight.js
@@ -1,8 +1,21 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const hyperledger = require("../scripts/hyperledger");
 
-router.post('/', function(req, res) {
-  res.send('updateFlightHours endpoint');
+router.get("/", async function(req, res) {
+  if (
+    (Object.keys(req.body).length !== 2 || !req.body.tailNumber,
+    !req.body.hours)
+  ) {
+    return res.sendStatus(400);
+  }
+  let companies = await hyperledger.invoke("mychannel", "airlineMRO", [
+    "updateFlightHours",
+    req.body.tailNumber,
+    req.body.hours
+  ]);
+  console.log(companies);
+  res.send(companies);
 });
 
 module.exports = router;
