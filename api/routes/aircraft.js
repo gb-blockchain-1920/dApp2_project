2538fffa97eeb4f106bded8c3bfaa8893f6ab897
aalu1418 <aalu1418@gmail.com>
2020-04-11 13:53:30 -0400

    register + get aircraft endpoitns

--- api/routes/aircraft.js
+++ api/routes/aircraft.js
@@ -1,16 +1,48 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-router.get('/', function(req, res) {
-  res.send('getAircraft endpoint');
+router.get("/", async function(req, res) {
+  try {
+    const aircraft = await hyperledger.query("mychannel", "airlineMRO", [
+      "getAircraft",
+      req.body.tailNumber,
+    ]);
+    res.send(aircraft);
+  } catch (e) {
+    console.log(e);
+    res.sendStatus(500);
+  }
 });
 
-router.post('/', function(req, res) {
-  res.send('registerAircraft endpoint');
+router.post("/", async function(req, res) {
+  console.log(req.body);
+  if (
+    Object.keys(req.body).length !== 3 ||
+    !req.body.aircraft ||
+    !req.body.tailNumber ||
+    !req.body.company
+  ) {
+    return res.sendStatus(400);
+  }
+  //pre-processing
+  req.body.company = req.body.company.toLowerCase();
+
+  try {
+    await hyperledger.invoke("mychannel", "airlineMRO", [
+      "registerAircraft",
+      req.body.aircraft,
+      req.body.tailNumber,
+      req.body.company
+    ]);
+    res.sendStatus(200);
+  } catch (e) {
+    console.log(e);
+    res.sendStatus(500);
+  }
 });
 
-router.patch('/', function(req, res) {
-  res.send('performMaintenance + replaceParts endpoint');
+router.patch("/", async function(req, res) {
+  res.send("performMaintenance + replaceParts endpoint");
 });
 
 module.exports = router;
