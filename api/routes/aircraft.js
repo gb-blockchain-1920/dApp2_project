4a53293bd41fb1295ed9cd16697933c4aa1333d6
aalu1418 <aalu1418@gmail.com>
2020-04-11 14:16:31 -0400

    patch endpoint for tracking maintenance

--- api/routes/aircraft.js
+++ api/routes/aircraft.js
@@ -6,7 +6,7 @@ router.get("/", async function(req, res) {
   try {
     const aircraft = await hyperledger.query("mychannel", "airlineMRO", [
       "getAircraft",
-      req.body.tailNumber,
+      req.body.tailNumber
     ]);
     res.send(aircraft);
   } catch (e) {
@@ -43,7 +43,38 @@ router.post("/", async function(req, res) {
 });
 
 router.patch("/", async function(req, res) {
-  res.send("performMaintenance + replaceParts endpoint");
+  console.log(req.body);
+  if (
+    Object.keys(req.body).length !== 4 ||
+    !req.body.tailNumber ||
+    !req.body.type ||
+    !req.body.notes
+  ) {
+    return res.sendStatus(400);
+  }
+
+  try {
+    //if there are replaced parts, call replace parts function
+    if (Object.keys(req.body.replacedParts).length > 0) {
+      await hyperledger.invoke("mychannel", "airlineMRO", [
+        "replaceParts",
+        req.body.tailNumber,
+        JSON.stringify(req.body.replacedParts)
+      ]);
+    }
+    //update maintenance
+    await hyperledger.invoke("mychannel", "airlineMRO", [
+      "performMaintenance",
+      req.body.tailNumber,
+      req.body.type,
+      req.body.notes,
+      JSON.stringify(req.body.replacedParts)
+    ]);
+    res.sendStatus(200);
+  } catch (e) {
+    console.log(e);
+    res.sendStatus(500);
+  }
 });
 
 module.exports = router;
