37354b30fedc52e813705fff40e0605f036956dc
aalu1418 <aalu1418@gmail.com>
2020-04-11 15:19:33 -0400

    administrative endpoint

--- api/routes/admin.js
+++ api/routes/admin.js
@@ -1,16 +1,68 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-router.get('/', function(req, res) {
-  res.send('getMaintainers endpoint');
+router.get("/", async function(req, res) {
+  console.log(req.body);
+  if (Object.keys(req.body).length !== 1 || !req.body.company) {
+    return res.sendStatus(400);
+  }
+
+  try {
+    const maintainers = await hyperledger.query("mychannel", "airlineMRO", [
+      "getMaintainers",
+      req.body.company
+    ]);
+    res.send(maintainers);
+  } catch (e) {
+    console.log(e);
+    res.sendStatus(500);
+  }
 });
 
-router.post('/', function(req, res) {
-  res.send('assignAircraft endpoint');
+router.post("/", async function(req, res) {
+  if (
+    Object.keys(req.body).length !== 3 ||
+    !req.body.username ||
+    !req.body.tailNumber ||
+    !req.body.company
+  ) {
+    return res.sendStatus(400);
+  }
+
+  try {
+    await hyperledger.invoke("mychannel", "airlineMRO", [
+      "assignAircraft",
+      req.body.username,
+      req.body.tailNumber,
+      req.body.company
+    ]);
+    res.send(200);
+  } catch (e) {
+    console.log(e);
+    res.sendStatus(500);
+  }
 });
 
-router.patch('/', function(req, res) {
-  res.send('sellAircraft endpoint');
+router.patch("/", async function(req, res) {
+  if (
+    Object.keys(req.body).length !== 2 ||
+    !req.body.company ||
+    !req.body.tailNumber
+  ) {
+    return res.sendStatus(400);
+  }
+
+  try {
+    await hyperledger.invoke("mychannel", "airlineMRO", [
+      "sellAircraft",
+      req.body.tailNumber,
+      req.body.company
+    ]);
+    res.send(200);
+  } catch (e) {
+    console.log(e);
+    res.sendStatus(500);
+  }
 });
 
 module.exports = router;
