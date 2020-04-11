351785d11531f73413b86486ab3e969721e7376e
aalu1418 <aalu1418@gmail.com>
2020-04-11 15:22:55 -0400

    fixed missing dependency

--- api/routes/admin.js
+++ api/routes/admin.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var router = express.Router();
+const hyperledger = require("../scripts/hyperledger");
 
 router.get("/", async function(req, res) {
   console.log(req.body);
