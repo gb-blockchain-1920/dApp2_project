8ab694a09e1ff739aca9fee0d7395a3555b8e635
aalu1418 <aalu1418@gmail.com>
2020-04-11 14:40:33 -0400

    part functionality in API

--- api/routes/part.js
+++ api/routes/part.js
@@ -1,12 +1,46 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const hyperledger = require("../scripts/hyperledger");
 
-router.get('/', function(req, res) {
-  res.send('getPart endpoint');
+router.get("/", async function(req, res) {
+  console.log(req.body);
+  if (!req.body.partID) {
+    return res.sendStatus(400);
+  }
+
+  try {
+    await hyperledger.invoke("mychannel", "airlineMRO", [
+      "getPart",
+      req.body.partID.toString()
+    ]);
+    res.sendStatus(200);
+  } catch (e) {
+    console.log(e);
+    res.sendStatus(500);
+  }
 });
 
-router.post('/', function(req, res) {
-  res.send('newPart endpoint');
+router.post("/", async function(req, res) {
+  console.log(req.body);
+  if (
+    Object.keys(req.body).length >= 2 ||
+    !req.body.description.id ||
+    !req.body.description.name ||
+    !req.body.maximumHours
+  ) {
+    return res.sendStatus(400);
+  }
+
+  try {
+    await hyperledger.invoke("mychannel", "airlineMRO", [
+      "newPart",
+      JSON.stringify(req.body)
+    ]);
+    res.sendStatus(200);
+  } catch (e) {
+    console.log(e);
+    res.sendStatus(500);
+  }
 });
 
 module.exports = router;
